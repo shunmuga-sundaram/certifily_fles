a6c48a9a0431efbb4b8fc2256e5be83e29dab007
bwselakkiya <elakkiyasundaram@blaze.ws>
2025-11-24 17:52:04 +0530

    fix:cors issue

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import getDataSource from "./db/data-source";
 import certRoute from "./routes/certRoute";
+import cors from 'cors';
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send({ message: "Welcome to Certifi.ly" });
